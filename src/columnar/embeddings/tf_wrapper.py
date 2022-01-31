dd924af9bd77ebd2f6a17bf4d3c104408e0b78dc
Sylvain Payot <97038121+spayot@users.noreply.github.com>
2022-01-31 08:27:36 -0800

    starting tf wrapper

--- src/columnar/embeddings/tf_wrapper.py
+++ src/columnar/embeddings/tf_wrapper.py
@@ -1,5 +1,9 @@
 """Wrapper Class to fit an embedding layer based on a 
-simple Deep Learning model."""
+simple Deep Learning model.
+
+how to do proper get_params
+
+"""
 from typing import Optional
 
 import pandas as pd 
@@ -14,24 +18,39 @@ from .tf_models import TFCatEmbsModel
 
 
 class TFEmbeddingWrapper(CategoricalEncoder):
+    """Allows to transform categorical features into embeddings, 
+    after fitting a simple neural network on the dataset.
+    Note: numerical features are normalized for the sake of fitting 
+    the DNN and generating embeddings."""
     def __init__(self, 
-                 dataframe: pd.DataFrame,
+                 task: str,
                  features: FeatureSelection,
-                 **kwargs,
+                 root_path: str = './',
                 ):
+        self.ROOT_PATH = root_path
         self.features = features
+        self.task = task
+        
+        # load the dataset for the task
+        loader = col.DataLoader(root=root_path, task=task)
+        dataframe = loader.load_data()
         dataset = df_to_dataset(dataframe, 
                                 target=features.target,
-                                **kwargs,
                                 )
+        
+        # initialize model (concatenated embeddings + dense layers) based on dataset and feature selection
         self.model = TFCatEmbsModel(dataset, features)
+        
+        # compile
         self.model.compile(optimizer='adam',
                            loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
                            metrics=["accuracy"])
-    
+        
+        # 
+        self.model.fit(dataset)
     
     def get_params(self, deep: bool):
-        pass
+        return {'task': self.task, 'features': self.features, 'root_path': self.root_path}
     
     
     def fit(self, df: pd.DataFrame, y: Optional[pd.Series] = None) -> None:
